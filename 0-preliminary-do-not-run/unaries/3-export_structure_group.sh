cf5849bd178c00447900381b461843ae047e73a2
JaquieLaMitaine <younous.maillefer@epfl.ch>
2025-06-13 19:06:49 +0200

    Remove all files

--- 0-preliminary-do-not-run/unaries/3-export_structure_group.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SET_NAME=unaries-verification-PBE-v1
-
-verdi archive create -G acwf-verification/${SET_NAME}/structures -- acwf-verification_${SET_NAME}_structures.aiida
--- 0-preliminary-do-not-run/unaries/old-sets/README.txt
+++
@@ -1,1 +0,0 @@
-NOTE: `set2` was considered to be good enough and it became the final set `unaries-verification-PBE-v1`
